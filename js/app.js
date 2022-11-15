757c912b66d84530eb1a51ae6d15851501146be0
Xeltax <85458421+Xeltax@users.noreply.github.com>
2022-11-15 10:19:05 +0100

    some modification
    changing icon to clickable icon and adding js on portfolio

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'js/particles.json', function() {
-    console.log('callback - particles.js config loaded');
 });
---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+function openPopup() {
+    let popup = document.querySelector(".popup")
+
+    popup.style.visibility = "visible";
+}
+
+function hidePopup() {
+    let popup = document.querySelector(".popup")
+
+    popup.style.visibility = "hidden";
+}
